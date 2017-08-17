adec89ca82c4d7fd2fc5553c3b04c732e7e4ba92
Nicolas <nrestrepot@unal.edu.co>
2017-08-17 20:08:44 +0000

    Controller welcome and predefined route added

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+class WelcomeController < ApplicationController
+    #.html.rb
+    def index
+        puts "Hola mundo en consola"
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get '/welcome', to:'welcome#index' #When someome clicks welcome it sends to the welcomecontroller and execute index
+  #solo para rutas, no hay logica aca porque están en los controladores
+  root 'welcome#index'
 end
