7805f34f0d8a2af81729e8654f0d173ca23e078d
Nicolas <nrestrepot@unal.edu.co>
2017-08-17 20:27:36 +0000

    First view and controller made in class

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,10 @@ class WelcomeController < ApplicationController
     #.html.rb
     def index
         puts "Hola mundo en consola"
+        @imprimir = false
+        a = 5 #local variable, all other methods wihthin controllar can't acess
+        @nombre = "David Camilo" #This variable is seen for index, but other "actions" of the controller either not
+        @@nooombre  = "Ds" #variable global
+        b = a+1
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'about', to:'index#about'
+
+  get '/contact', to:'index#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get '/welcome', to:'welcome#index' #When someome clicks welcome it sends to the welcomecontroller and execute index
   #solo para rutas, no hay logica aca porque están en los controladores
